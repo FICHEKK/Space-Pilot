e41e7990029646a1d8af15650e7c8d63d1bd995b
FICHEKK <filipnemec3@gmail.com>
2022-01-05 13:17:45 +0100

    Make spawning infinite and improve map settings.

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -6,26 +6,56 @@ public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField] private MapSettings mapSettings;
     [SerializeField] private Transform spaceship;
-    [SerializeField] private float distanceBetweenAsteroidFronts;
-    [SerializeField] private int initialAsteroidFrontCount;
-    [SerializeField] private int startAsteroidFrontIndex;
     [SerializeField] private List<GameObject> asteroidPrefabs;
 
-    private void Start() => SpawnInitialAsteroidFronts();
+    private GameObject _fronts;
+    private float _lastFrontDistanceFromOrigin;
 
-    private void SpawnInitialAsteroidFronts()
+    private void Start()
     {
-        for (var i = startAsteroidFrontIndex; i < startAsteroidFrontIndex + initialAsteroidFrontCount; i++)
-        {
-            var asteroidCount = Random.Range(0, mapSettings.laneCount);
+        _fronts = new GameObject("Fronts");
+        _lastFrontDistanceFromOrigin = mapSettings.firstFrontDistanceFromOrigin;
 
-            foreach (var laneIndex in GetRandomLaneIndices(asteroidCount))
-            {
-                var position = new Vector3(laneIndex * mapSettings.laneWidth, 0, distanceBetweenAsteroidFronts * i);
-                var asteroid = Instantiate(asteroidPrefabs[Random.Range(0, asteroidPrefabs.Count)], position, Quaternion.identity);
-                asteroid.transform.localScale = new Vector3(Random.Range(2, 3f), Random.Range(2, 3f), Random.Range(2, 3f));
-            }
+        for (var i = 0; i < mapSettings.frontCount; i++)
+        {
+            SpawnNextFront();
         }
+    }
+
+    private void Update()
+    {
+        if (spaceship == null ||_fronts.transform.childCount == 0) return;
+
+        var firstFront = _fronts.transform.GetChild(0);
+        if (spaceship.position.z < firstFront.position.z) return;
+
+        Destroy(firstFront.gameObject);
+        SpawnNextFront();
+    }
+
+    private void SpawnNextFront()
+    {
+        var front = new GameObject("Front");
+        front.transform.parent = _fronts.transform;
+        front.transform.position = new Vector3(0, 0, _lastFrontDistanceFromOrigin);
+
+        foreach (var laneIndex in GetRandomLaneIndices(laneCount: Random.Range(mapSettings.minAsteroidCountPerFront, mapSettings.maxAsteroidCountPerFront + 1)))
+        {
+            var asteroid = Instantiate(
+                original: asteroidPrefabs[Random.Range(0, asteroidPrefabs.Count)],
+                position: new Vector3(laneIndex * mapSettings.laneWidth, 0, front.transform.position.z),
+                rotation: Quaternion.identity,
+                parent: front.transform
+            );
+
+            asteroid.transform.localScale = new Vector3(
+                Random.Range(mapSettings.minAsteroidScale.x, mapSettings.maxAsteroidScale.x),
+                Random.Range(mapSettings.minAsteroidScale.y, mapSettings.maxAsteroidScale.y),
+                Random.Range(mapSettings.minAsteroidScale.z, mapSettings.maxAsteroidScale.z)
+            );
+        }
+
+        _lastFrontDistanceFromOrigin += Random.Range(mapSettings.minDistanceBetweenFronts, mapSettings.maxDistanceBetweenFronts);
     }
 
     private IEnumerable<int> GetRandomLaneIndices(int laneCount)
--- Assets/Scripts/MapSettings.cs
+++ Assets/Scripts/MapSettings.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Map Settings", menuName = "Map Settings", order = 0)]
 public class MapSettings : ScriptableObject
 {
+    [Header("Lane settings")]
     public int laneCount;
     public float laneWidth;
+
+    [Header("Front settings")]
+    public int frontCount;
+    public float firstFrontDistanceFromOrigin;
+    public float minDistanceBetweenFronts;
+    public float maxDistanceBetweenFronts;
+    public int minAsteroidCountPerFront;
+    public int maxAsteroidCountPerFront;
+
+    [Header("Asteroid settings")]
+    public Vector3 minAsteroidScale;
+    public Vector3 maxAsteroidScale;
 }
